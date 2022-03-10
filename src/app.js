d8ddf8c6fe6cc70ba92d5dd883dcdc439c0627d8
Zian Waqas Javed <zianjaved@hotmail.com>
2022-03-10 22:41:48 +0000

    finshed adding 'update and delete' in mongoose database

--- src/app.js
+++ src/app.js
@@ -1,26 +1,34 @@
 require ("./db/connection");
 const yargs = require("yargs");
-const {addMovie, list} = require("./movie/functions")
+const {addMovie, list, update} = require("./movie/functions")
 const mongoose = require("mongoose")
+const { addMovie, list, update, remove } = require('./movie/functions');
+
 
 const app = async (yargsObj) => {
     try {
-        if (yargsObj.add ) {
-            console.log(await addMovie(yargsObj.title, yargsObj.actor))
-            //add functionailty
+        if (yargsObj.addMovie) {
+            // Add a movie
+            console.log(await addMovie(yargsObj));
+        } else if (yargsObj.list) {
+            // List a specific movie
+            console.log(await list(yargsObj));
+        } else if (yargsObj.update) {
+            // Update a specific movie entry
+            await update(yargsObj);
+        } else if (yargsObj.delete) {
+            // Delete a specific movie entry
+            console.log(await remove(yargsObj));   
+        } else {
+            console.log("invalid command")
         }
-        else if (yargsObj.list){
-            console.log(await list());
-            //list functionailty
-        }
-        else {
-            console.log("Incorrect command")
-        }
-        await mongoose.disconnect()
+        console.log("Disconnecting...")
+        await mongoose.disconnect();
     } catch (error) {
         console.log(error)
     }
 }
+
 
 app(yargs.argv);
 
--- src/movie/functions.js
+++ src/movie/functions.js
@@ -1,20 +1,49 @@
+
 const Movie = require("./model");
 
-exports.addMovie = async (title, actor) => {
+exports.addMovie = async (obj) => {
     try {
-        
-        await Movie.create({title, actor});
-        return "Successfully added"
+        await Movie.create({title: obj.title, actor: obj.actor});
+        return `Successfully added ${obj.title}`;
     } catch (error) {
-        console.log(error)
-        
+        return `OOPS, there was an error: ${error}`
     }
 }
 
-exports.list = async () => {
+exports.list = async (obj=null) => {
     try {
-        return await Movie.find({});  
+        if (obj.title) {
+            return await Movie.find({title: obj.title});
+        } else if (obj.actor) {
+            console.log(`Finding movies with actor ${obj.actor}`)
+            return await Movie.find({actor: obj.actor})
+        } else {
+            console.log("Fetching all movies...")
+            return await Movie.find({});
+        }
     } catch (error) {
-        console.log(error)
+        return console.log(error);
     }
 }
+
+exports.update = async (obj) => {
+    try {
+        const query = { title: obj.title }
+        return await Movie.findOneAndUpdate( query, {actor: obj.actor} )
+    } catch (error) {
+        return console.log(error);
+    }
+}
+
+exports.delete = async (obj) => {
+    try {
+        if (obj.title) {
+            await Movie.findOneAndDelete({title: obj.title});
+            return `${obj.title} has been deleted from the database.`
+        }
+    } catch (error) {
+        return console.log(error);
+    }
+}
+
+
